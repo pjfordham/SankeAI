9ae10b45666b97f41cc3bdd67197f9f649bfb271
Peter Fordham <peter.fordham@gmail.com>
2023-01-20 22:58:30 -0800

    Remove seed_seq for simplicity.

--- food.h
+++ food.h
@@ -18,16 +18,14 @@ struct Pos {
 
 class FoodList {
    std::uniform_int_distribution<int> randomLocationRange;
-   std::seed_seq seq;
    std::mt19937 randomNumbers;
 
-   unsigned int seed;
    public:
+   unsigned int seed;
 
    FoodList( const FoodList & that ) :
       randomLocationRange{ that.randomLocationRange },
-      seq{ that.seed },
-      randomNumbers{ seq },
+      randomNumbers( that.seed ),
       seed{ that.seed } {
       // This copy constructor will restart the list from it's initial state
    }
@@ -35,15 +33,13 @@ class FoodList {
    FoodList& operator= ( const FoodList &that ) {
       randomLocationRange = that.randomLocationRange;
       seed = that.seed;
-      std::seed_seq _seq{ seed };
-      randomNumbers.seed( _seq );
+      randomNumbers.seed( seed );
       return *this;
    }
 
    FoodList(unsigned int _seed = 0xDEADBEEF, int lowRange = 0, int highRange = 37) :
       randomLocationRange{ lowRange, highRange },
-      seq{ _seed },
-      randomNumbers{ seq },
+      randomNumbers{ _seed },
       seed( _seed ) {
    }
 
